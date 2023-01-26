6e8a19d44a0173d8a32a38f0137495a59212b447
a-CID-man <118633755+a-CID-man@users.noreply.github.com>
2023-01-26 11:04:10 -0500

    start

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+     public int speed;
+
+     // Start is called before the first frame update
+     void Start()
+    {
+          speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          GameObject dan = 
+    }
+}
